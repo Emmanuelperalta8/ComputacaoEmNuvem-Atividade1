3c5d0e6ef2d24b266c4a2b16ed199a569f8e641b
SeuNomeEmmanuelPeralta <emmanuelperalta8@gmail.com>
2025-08-27 00:01:43 -0300

    upload de arquivos

--- django/src/core/urls.py
+++ django/src/core/urls.py
@@ -3,12 +3,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from uploads.views import upload_file
+
 
 def home(request):
     return HttpResponse("Django está rodando no Docker! 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upload/', upload_file),  # rota de upload
     path('', home),  # Adiciona a rota para "/"
 ]
 
---
+++ django/src/uploads/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .forms import UploadFileForm
+from django.conf import settings
+import os
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_obj = form.cleaned_data['file']
+            save_path = os.path.join(settings.MEDIA_ROOT, file_obj.name)
+            with open(save_path, 'wb+') as destination:
+                for chunk in file_obj.chunks():
+                    destination.write(chunk)
+            return render(request, 'uploads/success.html', {'filename': file_obj.name})
+    else:
+        form = UploadFileForm()
+    return render(request, 'uploads/upload.html', {'form': form})
